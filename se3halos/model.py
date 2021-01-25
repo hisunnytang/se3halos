24c8d553157fd73a8e38dd76e6e40b178b4e1c7d
root <hisunnytang@gmail.com>
2021-01-25 19:52:40 +0000

    add an architecture that output both scalar and se3 equivariant vector

--- se3halos/model.py
+++ se3halos/model.py
@@ -85,6 +85,78 @@ class SE3Transformer(nn.Module):
         return h
 
 
+class SE3Transformer_Vector(nn.Module):
+    def __init__(self,  num_layers: int, atom_feature_size: int, num_channels: int, num_nlayers: int=1, num_degrees: int=4, 
+                 edge_dim: int=4, div: float=4, pooling: str='avg', n_heads: int=1, **kwargs):
+        super().__init__()
+        # Build the network
+        self.num_layers = num_layers
+        self.num_nlayers = num_nlayers
+        self.num_channels = num_channels
+        self.num_degrees = num_degrees
+        self.edge_dim = edge_dim
+        self.div = div
+        self.pooling = pooling
+        self.n_heads = n_heads
+
+        self.fibers = {'in': Fiber(2, atom_feature_size),
+                       'mid': Fiber(num_degrees, self.num_channels),
+                       'out': Fiber(2, num_degrees*self.num_channels)}
+
+        blocks = self._build_gcn(self.fibers, 1)
+        self.Gblock, self.FCblock = blocks
+
+    def _build_gcn(self, fibers, out_dim):
+        # Equivariant layers
+        Gblock = []
+        fin = fibers['in']
+        for i in range(self.num_layers):
+            Gblock.append(GSE3Res(fin, fibers['mid'], edge_dim=self.edge_dim, 
+                                  div=self.div, n_heads=self.n_heads))
+            Gblock.append(GNormSE3(fibers['mid']))
+            fin = fibers['mid']
+        Gblock.append(GConvSE3(fibers['mid'], fibers['out'], self_interaction=True, edge_dim=self.edge_dim))
+
+        # expand the node-wise feature 
+        FCblock = []
+        f_in = Fiber(2, self.num_degrees*self.num_channels)
+        f_out = Fiber(2, 64)
+        FCblock.append(G1x1SE3(f_in,f_out))
+
+        # do another operation on the final pooled features
+        f_in = Fiber(2, 64)
+        f_out = Fiber(2, 2)
+        FCblock.append(G1x1SE3(f_in, f_out))
+
+
+        return nn.ModuleList(Gblock), nn.ModuleList(FCblock)
+
+    def forward(self, G):
+        # Compute equivariant weight basis from relative positions
+        basis, r = get_basis_and_r(G, self.num_degrees-1)
+
+        # encoder (equivariant layers)
+        logmass  = G.ndata['mass'].unsqueeze(-1)
+        velocity = G.ndata['velocity'].unsqueeze(1)
+        h = {'0': logmass,
+             '1': velocity}
+
+        for layer in self.Gblock:
+            h = layer(h, G=G, r=r, basis=basis)
+
+        # elementwise feature expansion
+        h = self.FCblock[0](h)
+
+        # aggregate the node features
+        if self.pooling == 'avg':
+          h['0'], h['1'] = h['0'].mean(axis=0), h['1'].mean(axis=0)
+        else:
+          h['0'], h['1'] = h['0'].max(axis=0), h['1'].max(axis=0)
+
+        h = self.FCblock[1](h)
+        return h
+
+
 if __name__ == '__main__':
     # pull the data
     haloDS = get_haloDataset()
